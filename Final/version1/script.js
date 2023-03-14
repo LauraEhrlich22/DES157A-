fbf30dff0c3a1b4621c33b72692340bea4e190be
Laura Ehrlich <lzehrlich@gmail.com>
2023-03-14 01:40:10 -0700

    user test

--- Final/version1/script.js
+++ Final/version1/script.js
@@ -3,9 +3,9 @@
     console.log("reading.js");
 
     var page1 = document.querySelector(".page1");
-    var page2 = document.querySelector("form");
+    var page2 = document.querySelector(".page2");
     var start = document.querySelector(".start");
-    // page1.classList.remove("hide");
+    page1.classList.remove("hide");
     
     start.addEventListener("click", function(){
         page2.classList.remove("hide");
